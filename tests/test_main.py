89f43cc58fa46224726192f938d94e2a1a35dcc2
tryadovoi <timaryadovou@yandex.ru>
2026-02-23 17:12:46 +0300

    add tests

---
+++ tests/test_main.py
@@ -0,0 +1,22 @@
+from fastapi.testclient import TestClient  # type: ignore
+from backend.main import app  # type: ignore
+
+
+client = TestClient(app)
+
+
+def test_health_check():
+    response = client.get("/health")
+    assert response.status_code == 200
+    assert response.json() == {"status": "ok"}
+
+def test_register_and_login():
+    user_data = {"login": "user", "password": "user"}
+    response = client.post("/register", json=user_data)
+    assert response.status_code == 200
+    assert "access_token" in response.json()
+
+    login_data = {"username": "user", "password": "user"}
+    response = client.post("/login", data=login_data)
+    assert response.status_code == 200
+    assert "access_token" in response.json()
