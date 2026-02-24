6ce6f5f4484b21ef4284285523c50aa7d4bcd36c
tryadovoi <timaryadovou@yandex.ru>
2026-02-24 22:29:51 +0300

    update ci v3 -- try 13

--- backend/db/database.py
+++ backend/db/database.py
@@ -5,18 +5,7 @@ import os
 
 
 DATABASE_URL = os.getenv("DATABASE_URL")
-TESTING = os.getenv("TESTING", "false").lower() == "true"
-
-if TESTING or not DATABASE_URL:
-    DATABASE_URL = "sqlite:///./test.db"
-    engine = create_engine(
-        DATABASE_URL,
-        connect_args={"check_same_thread": False}  
-    )
-else:
-    engine = create_engine(DATABASE_URL)
-
-# engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
